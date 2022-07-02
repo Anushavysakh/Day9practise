5d77255c34001ebb36c337074d3b7e32e2078f93
Anusha <anushavysakh232@gmail.com>
2022-07-02 22:18:21 +0530

    Employee details

---
+++ uc1.sh
@@ -0,0 +1,9 @@
+echo  "Welcome to Employee Wage Computation Program"
+echo "Attendence"
+result=$(($RANDOM%2))
+if [ $result -eq 0 ]
+then
+	echo "Employee is absent";
+else
+	echo "Employee is present";
+fi
